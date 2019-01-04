f96c3ad0991858c02899fab0e1cbf219ccc4a4c2
JinHwanJung <yadot1478@naver.com>
2019-01-04 10:47:09 +0900

    풀이완료.

---
+++ leetcode/283_move_zeroes.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        zero_count = nums.count(0)
+        for _ in range(zero_count):
+            nums.remove(0)
+            nums.append(0)
+
+    # def moveZeroes2(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: void Do not return anything, modify nums in-place instead.
+    #     """
+    #     pass
+
+def test():
+    s = Solution()
+    l = [0, 1, 0, 3, 12]
+    s.moveZeroes(l)
+    assert l == [1, 3, 12, 0, 0]
+
+# def test2():
+#     s = Solution()
+#     l = [0, 1, 0, 3, 12]
+#     s.moveZeroes(l)
+#     assert l == [1, 3, 12, 0, 0]
